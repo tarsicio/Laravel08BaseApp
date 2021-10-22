d6ae31f8e069bffe70bebd6c1adb098afb24fcc4
Tarsicio Carrizales <telecom.com.ve@gmail.com>
2021-10-21 21:24:01 -0400

    add footer, github

--- resources/views/vendor/adminlte/layouts/partials/footer.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -5,5 +5,5 @@
         <a href="https://github.com/tarsicio/Laravel08BaseApp"></a><b>Version 2.0</b></a> {{ trans('message.descriptionpackage') }}
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2016-2021 <a href="http://www.twitter.com/tarsicio_tic" target="_blank"><i class="fa fa-twitter"></i>Tarsicio Carrizales</a> {{ trans('message.createdby') }} <a href="https://ve.linkedin.com/in/tarsicioc" target="_blank"><i class="fa fa-linkedin"></i> Tarsicio Carrizales</a>, {{ trans('message.mensajes_alert.mit') }}</strong>, {{ trans('message.seecode') }} <a href="https://github.com/tarsicio/Laravel-User-Rol-Permission-CRUD" target="_blank"><b>Github/Tarsicio</b></a>
+    <strong>Copyright &copy; 2016-2021 <a href="http://www.twitter.com/tarsicio_tic" target="_blank"><i class="fa fa-twitter"></i>Tarsicio Carrizales</a> {{ trans('message.createdby') }} <a href="https://ve.linkedin.com/in/tarsicioc" target="_blank"><i class="fa fa-linkedin"></i> Tarsicio Carrizales</a>, {{ trans('message.mensajes_alert.mit') }}</strong>, {{ trans('message.seecode') }} <a href="https://github.com/tarsicio/Laravel-User-Rol-Permission-CRUD" target="_blank"> <i class="fa fa-github"></i><b> Github</b></a>
 </footer>
